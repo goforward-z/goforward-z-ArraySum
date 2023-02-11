c61e094fac5e43899c1eca8acef7be17132cefb1
Lepeng Z <zlepeng.ucf@gmail.com>
2023-02-11 20:55:30 +0000

    done ArraySum challenge

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,12 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
 
-        return 0;
+            sum += arr[i];
+        }
+
+        return sum;
     }
 }
